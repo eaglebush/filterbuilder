8d46524420b6fc8720464bd641893fea0bcd8c2a
Elizalde Baguinon <mizmerize@yahoo.com>
2023-10-27 09:22:26 +0800

    Update pkg dependencies

--- filterbuilder.go
+++ filterbuilder.go
@@ -43,7 +43,6 @@ type FieldTypeConstraint interface {
 
 // NewFilter creates a new Filter object
 func NewFilter(eq []Pair, paramInSequence bool, paramPlaceHolder string) *Filter {
-
 	if paramPlaceHolder == "" {
 		if paramInSequence {
 			paramPlaceHolder = "@p"
@@ -51,7 +50,6 @@ func NewFilter(eq []Pair, paramInSequence bool, paramPlaceHolder string) *Filter
 			paramPlaceHolder = "?"
 		}
 	}
-
 	return &Filter{
 		Eq:                   eq,
 		ParameterInSequence:  paramInSequence,
@@ -74,7 +72,6 @@ func RawPair(column string, value interface{}) Pair {
 // RawMultiPair simplifies raw multi-pair.
 // Pairs reads the value argument raw.
 func RawMultiPair(column string, value ...interface{}) MultiFieldPair {
-
 	v := make([]Value, 0, len(value))
 	for _, a := range value {
 		v = append(v, Value{
@@ -82,7 +79,6 @@ func RawMultiPair(column string, value ...interface{}) MultiFieldPair {
 			Src: a,
 		})
 	}
-
 	return MultiFieldPair{
 		Column: column,
 		Value:  v,
@@ -103,14 +99,12 @@ func DataPair(column string, fieldName string) Pair {
 // DataMultiPair simplifies data multi-pair.
 // Pairs reads the Data field values via fieldName argument.
 func DataMultiPair(column string, fieldName ...string) MultiFieldPair {
-
 	v := make([]Value, 0, len(fieldName))
 	for _, a := range fieldName {
 		v = append(v, Value{
 			Src: a,
 		})
 	}
-
 	return MultiFieldPair{
 		Column: column,
 		Value:  v,
@@ -155,7 +149,6 @@ func (fb *Filter) Build() ([]string, []interface{}, error) {
 		len(fb.Lk) == 0 &&
 		len(fb.Between) == 0 &&
 		!fb.AllowNoFilters {
-
 		return sql, args, ErrNoFilterSet
 	}
 
@@ -163,16 +156,13 @@ func (fb *Filter) Build() ([]string, []interface{}, error) {
 
 	// Get Equality filters
 	for _, sv := range fb.Eq {
-
 		v, err = fb.Value(sv.Value)
 		if err != nil {
 			return sql, args, err
 		}
-
 		if v == nil {
 			continue
 		}
-
 		switch v.(type) {
 		case Null:
 			tmp = sv.Column + " IS NULL"
@@ -182,26 +172,20 @@ func (fb *Filter) Build() ([]string, []interface{}, error) {
 			if fb.ParameterInSequence {
 				tmp += strconv.Itoa(fb.ParameterOffset)
 			}
-
 			args = append(args, v)
 		}
-
 		sql = append(sql, tmp)
-
 	}
 
 	// Get  Non-Equality filters
 	for _, sv := range fb.Ne {
-
 		v, err = fb.Value(sv.Value)
 		if err != nil {
 			return sql, args, err
 		}
-
 		if v == nil {
 			continue
 		}
-
 		switch v.(type) {
 		case Null:
 			tmp = sv.Column + " IS NOT NULL"
@@ -213,30 +197,23 @@ func (fb *Filter) Build() ([]string, []interface{}, error) {
 			}
 			args = append(args, v)
 		}
-
 		sql = append(sql, tmp)
 	}
 
 	// Get Like filters
 	for _, sv := range fb.Lk {
-
 		v, err = fb.Value(sv.Value)
 		if err != nil {
 			return sql, args, err
 		}
-
 		if v == nil {
 			continue
 		}
-
 		fb.ParameterOffset++
-
 		tmp = sv.Column + " LIKE " + fb.ParameterPlaceholder
-
 		if fb.ParameterInSequence {
 			tmp += strconv.Itoa(fb.ParameterOffset)
 		}
-
 		sql = append(sql, tmp)
 		args = append(args, v)
 	}
@@ -248,61 +225,48 @@ func (fb *Filter) Build() ([]string, []interface{}, error) {
 
 	// Get In filters
 	for _, sv := range fb.In {
-
 		vs, err = fb.getMultiPairValue(sv.Value)
 		if err != nil {
 			return sql, args, err
 		}
-
 		tmp = sv.Column + " IN (%s) "
-
 		cma = ""
 		prms = ""
 		for _, vx := range vs {
-
 			if v == nil {
 				continue
 			}
-
 			fb.ParameterOffset++
 			prms += cma + fb.ParameterPlaceholder
 			if fb.ParameterInSequence {
 				prms += strconv.Itoa(fb.ParameterOffset)
 			}
 			cma = ","
-
 			args = append(args, vx)
 		}
-
 		tmp = fmt.Sprintf(tmp, prms)
 		sql = append(sql, tmp)
 	}
 
 	// Get Not In filters
 	for _, sv := range fb.NotIn {
-
 		vs, err = fb.getMultiPairValue(sv.Value)
 		if err != nil {
 			return sql, args, err
 		}
-
 		tmp = sv.Column + " NOT IN (%s) "
-
 		cma = ""
 		prms = ""
 		for _, vx := range vs {
-
 			if v == nil {
 				continue
 			}
-
 			fb.ParameterOffset++
 			prms += cma + fb.ParameterPlaceholder
 			if fb.ParameterInSequence {
 				prms += strconv.Itoa(fb.ParameterOffset)
 			}
 			cma = ","
-
 			args = append(args, vx)
 		}
 
@@ -312,29 +276,23 @@ func (fb *Filter) Build() ([]string, []interface{}, error) {
 
 	// Get Between filters
 	for _, sv := range fb.Between {
-
 		vs, err = fb.getMultiPairValue(sv.Value)
 		if err != nil {
 			return sql, args, err
 		}
-
 		tmp = sv.Column + " BETWEEN %s "
-
 		cma = ""
 		prms = ""
 		for i, vx := range vs {
-
 			if v == nil {
 				continue
 			}
-
 			fb.ParameterOffset++
 			prms += cma + fb.ParameterPlaceholder
 			if fb.ParameterInSequence {
 				prms += strconv.Itoa(fb.ParameterOffset)
 			}
 			cma = " AND "
-
 			args = append(args, vx)
 
 			// break because between only accepts 2
@@ -342,65 +300,53 @@ func (fb *Filter) Build() ([]string, []interface{}, error) {
 				break
 			}
 		}
-
 		tmp = fmt.Sprintf(tmp, prms)
 		sql = append(sql, tmp)
-
 	}
-
 	return sql, args, nil
 }
 
 // ValueFor gets the value of the filter by column lookup
 func (fb *Filter) ValueFor(col string) (interface{}, error) {
-
 	for _, v := range fb.Eq {
 		if strings.EqualFold(v.Column, col) {
 			return fb.Value(v.Value)
 		}
 	}
-
 	for _, v := range fb.Ne {
 		if strings.EqualFold(v.Column, col) {
 			return fb.Value(v.Value)
 		}
 	}
-
 	for _, v := range fb.Lk {
 		if strings.EqualFold(v.Column, col) {
 			return fb.Value(v.Value)
 		}
 	}
-
 	for _, v := range fb.In {
 		if strings.EqualFold(v.Column, col) {
 			return fb.getMultiPairValue(v.Value)
 		}
 	}
-
 	for _, v := range fb.NotIn {
 		if strings.EqualFold(v.Column, col) {
 			return fb.getMultiPairValue(v.Value)
 		}
 	}
-
 	for _, v := range fb.Between {
 		if strings.EqualFold(v.Column, col) {
 			return fb.getMultiPairValue(v.Value)
 		}
 	}
-
 	return nil, ErrColumnNotFound
 }
 
 // ValueFor gets the value of the filter by column lookup that automatically
 func ValueFor[T FieldTypeConstraint](fb *Filter, col string) (T, error) {
-
 	ifc, err := fb.ValueFor(col)
 	if err != nil {
 		return *new(T), err
 	}
-
 	// get value thru reflect
 	var vx interface{}
 	t := reflect.ValueOf(ifc)
@@ -412,83 +358,65 @@ func ValueFor[T FieldTypeConstraint](fb *Filter, col string) (T, error) {
 	} else {
 		vx = t.Interface()
 	}
-
 	val, ok := vx.(T)
 	if !ok {
 		return *new(T), ErrDataAssertionMismatch
 	}
-
 	return val, err
 }
 
 // Weld joins an existing SQL string and its arguments with the results from the Build function
 func (fb *Filter) Weld(sql string, args []interface{}, paramoffset int) (string, []interface{}, error) {
-
 	fb.ParameterOffset = paramoffset
-
 	fexp, fargs, err := fb.Build()
 	if err != nil {
 		return sql, args, err
 	}
-
 	if len(fexp) > 0 {
 		// remove trailing space and semi-colon
 		sql = strings.TrimSpace(sql)
 		sql = strings.TrimRight(sql, `;`)
-
 		sql += " WHERE " + strings.Join(fexp, " AND ")
 	}
-
 	if len(fargs) > 0 {
 		args = append(args, fargs...)
 	}
-
 	return sql, args, nil
 }
 
 // Value gets the actual value of the struct field or the raw value that has been set
 func (fb *Filter) Value(p Value) (interface{}, error) {
-
 	if p.Raw {
 		if p.Src == nil {
 			return Null(true), nil
 		}
-
 		rv := reflect.ValueOf(p.Src)
 		if rv.Kind() != reflect.Ptr {
 			return p.Src, nil
 		}
-
 		vv := rv.Elem()
 		if !vv.IsValid() {
 			return Null(true), nil
 		}
-
 		return p.Src, nil
 	}
-
 	if fb.Data == nil {
 		return nil, ErrDataNotSet
 	}
-
 	// get value thru reflect
 	t := reflect.ValueOf(fb.Data)
-
 	fld, ok := p.Src.(string)
 	if !ok {
 		return nil, ErrInvalidFieldName
 	}
-
 	if t.Kind() != reflect.Struct {
 		return nil, ErrDataIsNotStruct
 	}
-
 	f := t.FieldByNameFunc(func(s string) bool {
 		return strings.EqualFold(fld, s)
 	})
 
 	var vx interface{}
-
 	if f.Kind() == reflect.Ptr {
 		if fx := f.Elem(); !fx.IsValid() {
 			return nil, nil
@@ -497,7 +425,6 @@ func (fb *Filter) Value(p Value) (interface{}, error) {
 	} else {
 		vx = f.Interface()
 	}
-
 	return vx, nil
 }
 
@@ -512,29 +439,23 @@ func (fb *Filter) Valid() bool {
 }
 
 func (fb *Filter) getMultiPairValue(p []Value) ([]interface{}, error) {
-
 	var (
 		err  error
 		args []interface{}
 		v    interface{}
 	)
-
 	args = make([]interface{}, 0)
-
 	// get value thru reflect
 	t := reflect.TypeOf(fb.Data)
 	if t == nil {
 		return nil, ErrDataNotSet
 	}
-
 	for _, mv := range p {
 		v, err = fb.Value(mv)
 		if err != nil {
 			return args, err
 		}
-
 		args = append(args, v)
 	}
-
 	return args, nil
 }
--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.19
 
 require (
 	github.com/shopspring/decimal v1.3.1
-	golang.org/x/exp v0.0.0-20230210204819-062eb4c674ab
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
 )
